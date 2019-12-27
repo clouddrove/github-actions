f93b4afbcaab9e80c24af92424b221a5193c180d
Sohan Yadav <sohan@clouddrove.com>
2019-12-27 13:08:57 +0530

    add terratest

--- src/tf_init.sh
+++ src/tf_init.sh
@@ -3,21 +3,6 @@
 function terraformInit {
   # Gather the output of `terraform init`.
   echo "init: info: initializing Terraform configuration in ${tfWorkingDir}"
-  initOutput=$(terraform init -input=false ${*} 2>&1)
-  initExitCode=${?}
+  terraform init
 
-  # Exit code of 0 indicates success. Print the output and exit.
-  if [ ${initExitCode} -eq 0 ]; then
-    echo "init: info: successfully initialized Terraform configuration in ${tfWorkingDir}"
-    echo "${initOutput}"
-    echo
-    exit ${initExitCode}
-  fi
-
-  # Exit code of !0 indicates failure.
-  echo "init: error: failed to initialize Terraform configuration in ${tfWorkingDir}"
-  echo "${initOutput}"
-  echo
-
-  exit ${initExitCode}
 }
