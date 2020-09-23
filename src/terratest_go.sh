79104d6e5019da7749094d88bac7d6d7e127cc0c
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-09-23 20:19:33 +0530

    update

--- src/terratest_go.sh
+++ src/terratest_go.sh
@@ -10,7 +10,17 @@ function goTest {
     echo exists
 else
   echo "Install Go for terratest"
-  wget -q -O - https://raw.githubusercontent.com/clouddrove/github-actions/CD-89/src/install-go.sh | bash -s -- --version 1.13.2
+  echo "installing go version 1.10.3..." 
+  apk add --no-cache --virtual .build-deps bash gcc musl-dev openssl go 
+  wget -O go.tgz https://dl.google.com/go/go1.10.3.src.tar.gz 
+  tar -C /usr/local -xzf go.tgz 
+  cd /usr/local/go/src/ 
+  ./make.bash 
+  export PATH="/usr/local/go/bin:$PATH"
+  export GOPATH=/opt/go/ 
+  export PATH=$PATH:$GOPATH/bin 
+  apk del .build-deps 
+  go version
 
   echo "Install Go package fo terratest"
   go get github.com/gruntwork-io/terratest/modules/terraform github.com/stretchr/testify/assert
