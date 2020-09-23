661228f9ea7b0a844c5e4a5364e66c2e77ff84ef
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-09-23 20:57:29 +0530

    update

--- src/terratest_go.sh
+++ src/terratest_go.sh
@@ -12,14 +12,14 @@ else
   echo "Install Go for terratest"
   echo "installing go version 1.10.3..." 
   apk add --no-cache --virtual .build-deps bash gcc musl-dev openssl go 
-  curl -O https://storage.googleapis.com/golang/go1.4.3.linux-amd64.tar.gz
-  tar -C /usr/local -xzf go1.4.3.linux-amd64.tar.gz
+  curl -O https://golang.org/dl/go1.15.2.src.tar.gz
+  tar -C /usr/local -xzf go1.15.2.src.tar.gz
   cd /usr/local/go/src/ 
   ./make.bash 
   export PATH="/usr/local/go/bin:$PATH"
   export GOPATH=/opt/go/ 
   export PATH=$PATH:$GOPATH/bin 
-  apk del .build-deps 
+  
   go version
   echo starting
   go get github.com/gruntwork-io/terratest/modules/terraform github.com/stretchr/testify/assert
