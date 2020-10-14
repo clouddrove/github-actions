846040259c0ab2470e8fd7495b025b8c1e33eecc
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-10-14 21:13:31 +0530

    update

--- src/push.sh
+++ src/push.sh
@@ -3,8 +3,8 @@
 function push {
  
  
-  git config --global user.email "ajay@clouddrove.com"
-  git config --global user.name "Ajay dhyani"
+  git config --global user.email "anmol@clouddrove.com"
+  git config --global user.name "Anmol Nagpal"
   git add . && git commit -m "upload"
   git push origin master
 }
