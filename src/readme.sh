1ba5bcf392d68fe1ec31bfcda3841c5ee0a0508c
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-10-15 19:00:07 +0530

    update actions

--- src/readme.sh
+++ src/readme.sh
@@ -2,8 +2,11 @@
 
 function readme {
   export GITHUB_ACCESS_TOKEN=$1
+  cat $1
+  pwd
   cd .. && cd .. && cd ..
   git clone https://$1@github.com/clouddrove/genie.git
+  pwd
   cd /home/runner/work/ajay-testing/ajay-testing
   make packages/install/gomplate
   make readme
