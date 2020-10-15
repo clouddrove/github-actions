8f36aa0b3ee5c8c14de3a33acfd830d92badfb82
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-10-15 20:30:34 +0530

    updated completed- (push & readme scripts)

--- src/push.sh
+++ src/push.sh
@@ -5,5 +5,5 @@ function push {
   git config --global user.email "anmol@clouddrove.com"
   git config --global user.name "Anmol Nagpal"
   git add . && git commit -m "pushed README.md"
-  git push origin master 
+  git push origin master
 }
