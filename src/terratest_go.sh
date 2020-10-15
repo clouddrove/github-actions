0d0fb7169b1f17efded4e6856703e0249dcab246
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-10-15 17:17:04 +0530

    update actions

--- src/terratest_go.sh
+++ src/terratest_go.sh
@@ -5,6 +5,22 @@ function goTest {
   echo "install the terratest dependencies"
   apk add -d --update --no-cache go curl gcc build-base
 
+  echo "setup go"
+  if [ -x /github/home/.go ]; then
+    echo exists
+  else
+  echo "Install Go for terratest"
+  wget -O go.tgz https://dl.google.com/go/go1.10.3.src.tar.gz
+  tar -C /usr/local -xzf go.tgz
+  cd /usr/local/go/src/
+  ./make.bash
+  export PATH="/usr/local/go/bin:$PATH"
+  export GOPATH=/opt/go/
+  export PATH=$PATH:$GOPATH/bin
+  apk del .build-deps
+  go version
+  fi
+
   echo "Install Go dep for terratest"
   if [ -f Gopkg.toml ]; then
   curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
