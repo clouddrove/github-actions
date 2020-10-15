d5e02c584e81d7b473db371fdc4a1097f04e869a
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-10-15 17:33:36 +0530

    update actions

--- src/terratest_go.sh
+++ src/terratest_go.sh
@@ -10,14 +10,13 @@ function goTest {
     echo exists
   else
   echo "Install Go for terratest"
-  wget -O go.tgz https://dl.google.com/go/go1.10.3.src.tar.gz
+  wget -O go.tgz https://golang.org/dl/go1.15.3.linux-amd64.tar.gz
   tar -C /usr/local -xzf go.tgz
   cd /usr/local/go/src/
   ./make.bash
   export PATH="/usr/local/go/bin:$PATH"
   export GOPATH=/opt/go/
   export PATH=$PATH:$GOPATH/bin
-  apk del .build-deps
   go version
   fi
 
