b53d19bbc6a00a4ab7e68faadf04dbe70e36afd5
anmolnagpal <anmol@clouddrove.com>
2021-01-13 21:34:44 +0530

    upgrade terraform version

--- src/terratest_go.sh
+++ src/terratest_go.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 #this is using for terraform  terratest
 function goTest {
-  echo "install the terratest dependencies"
-  apk add -d --update --no-cache go curl gcc build-base
 
-  echo "setup go"
+  echo "install the terratest dependencies"
+  apk add -d --update --no-cache go gcc build-base
+
+  echo "Install Go for terratest"
   if [ -x /github/home/.go ]; then
     echo exists
-  else
+else
   echo "Install Go for terratest"
-  wget -O go.tgz https://golang.org/dl/go1.15.3.linux-amd64.tar.gz
-  tar -C /usr/local -xzf go.tgz
-  cd /usr/local/go/src/
-  ./make.bash
-  export PATH="/usr/local/go/bin:$PATH"
-  export GOPATH=/opt/go/
-  export PATH=$PATH:$GOPATH/bin
-  go version
-  fi
+  wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
+ | bash -s -- --version 1.15.6
 
-  echo "Install Go dep for terratest"
-  if [ -f Gopkg.toml ]; then
-  curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-  dep ensure
-  else
-  go get -v -t -d ./...
-  fi
+  echo "Install Go package fo terratest"
+  go get github.com/gruntwork-io/terratest/modules/terraform github.com/stretchr/testify/assert
+fi
 
+  # Gather the output of `teratest`.
   echo "teratest: info: teratest run configuration  in ${tfWorkingDir}"
-  cd /github/workspace
-  cd  ${tfWorkingDir}
-  go test  --timeout 15000m
+  go test -run Test --timeout 150m
 }
