abdaf39035a27fa23315930104f60a5cd8a23ec6
Sohan <sohan@clouddrove.com>
2021-01-14 19:31:48 +0530

    fix the terrtest

--- src/terratest_go.sh
+++ src/terratest_go.sh
@@ -2,21 +2,6 @@
 #this is using for terraform  terratest
 function goTest {
 
-  echo "install the terratest dependencies"
-  apk add -d --update --no-cache go gcc bash musl-dev openssl-dev ca-certificates tar && update-ca-certificates
-
-  echo "Install Go for terratest"
-  if [ -x /github/home/.go ]; then
-    echo exists
-else
-
- wget https://dl.google.com/go/go1.14.3.src.tar.gz && tar -C /usr/local -xzf go1.14.3.src.tar.gz
- cd /usr/local/go/src && ./make.bash
- export  PATH=$PATH:/usr/local/go/bin
-
-#we delete the apk installed version to avoid conflict
-   apk del go
-
   echo "Install Go package fo terratest"
   go get  github.com/gruntwork-io/terratest/modules/terraform
 fi
