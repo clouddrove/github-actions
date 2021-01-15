f2426190a6c9e70e1979461029b50a038aa6f46a
Sohan <sohan@clouddrove.com>
2021-01-15 11:55:08 +0530

    fix github action

--- src/terratest_go.sh
+++ src/terratest_go.sh
@@ -5,7 +5,6 @@ function goTest {
   echo "Install Go package fo terratest"
   go get  github.com/gruntwork-io/terratest/modules/terraform
 
-
   # Gather the output of `teratest`.
   echo "teratest: info: teratest run configuration  in ${tfWorkingDir}"
   go test -run Test --timeout 150m
