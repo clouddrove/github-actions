2fb3d2649c3434a28226a0eb35c93b2d89ac72bd
Tanveer143s <116706588+Tanveer143s@users.noreply.github.com>
2023-09-20 22:14:03 +0530

    feat: added toc.sh file

---
+++ toc/toc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+MODULES=$2
+USERNAME=$3
+EMAIL=$4
+git config --global user.name  "$USERNAME"
+git config --global user.email "$EMAIL"
+git config --global --add safe.directory /github/workspace/new-workflow
+export GITHUB_ACCESS_TOKEN=$1
+mkdir -p new-workflow
+cd .. && rsync -av --progress workspace/. /github/workspace/new-workflow --exclude new-workflow && cd /github/workspace/new-workflow
+cd .. && cd .. && cd ..
+git clone https://$1@github.com/clouddrove/genie.git
+pwd && ls -la
+cd /github/workspace/new-workflow
+make toc/deps
+make toc include_modules="$MODULES"
+git add .
+git commit -m "feat: updating toc readme"
+git push
