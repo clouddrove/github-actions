1e6ec58163e4a77d3fc32aa5a278aa7ac5b44daf
Vishwajit Nagulkar <119565952+VishwajitNagulkar@users.noreply.github.com>
2024-02-27 19:22:06 +0530

    feat: testing readme

--- src/readme.sh
+++ src/readme.sh
@@ -5,7 +5,7 @@ function readme {
   mkdir -p new-workflow
   cd .. && rsync -av --progress workspace/. /github/workspace/new-workflow --exclude new-workflow && cd /github/workspace/new-workflow
   cd .. && cd .. && cd ..
-  git clone https://$1@github.com/clouddrove/genie.git
+  git clone -b feat/readme https://$1@github.com/clouddrove/genie.git
   cd /github/workspace/new-workflow
   make packages/install/gomplate
   make readme
