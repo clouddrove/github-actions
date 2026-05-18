8782c8d7d3c8e4c0cbca6eded77f36f284c2dc78
Himanshu Ahirwar <himanshu.ahirwar@clouddrove.com>
2026-05-18 20:45:36 +0530

    fix: readme action for push subcommand (#23)

--- src/push.sh
+++ src/push.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 function push {
+  export GITHUB_ACCESS_TOKEN=$1
   cd /github/workspace/new-workflow
   git config --global --add safe.directory /github/workspace/new-workflow
   git config --global user.email "anmol+ci@clouddrove.com."
   git config --global user.name "clouddrove-ci"
-  git add . && git commit -m "update README.md"
+  git remote set-url origin https://x-access-token:${GITHUB_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+  git add .
+  git commit -m "update README.md" || true
   git push origin master
 }
